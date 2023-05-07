0dd12f478c7babddf24a7de6769064756af476fb
“battusairam” <battusairam6@gmail.com>
2023-05-07 09:46:15 +0530

    duplicate of orginal branch

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello{
 public static void main(String arg[]){
   System.out.println("HELLO world is first program");
 
-  System.out.println("HELLO WORLD is first program");
-System.out.println("HAI HELLO");
+  System.out.println("HELLO WORLD is FIRST program right");
+
 }}
